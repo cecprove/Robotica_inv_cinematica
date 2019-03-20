045a693eaa183cb80f8b6a1715b4f8feda5b3670
Axl31 <48432201+Axl31@users.noreply.github.com>
2019-03-20 11:57:36 +0100

    Update inv_cin_psd.m

--- inv_cin_psd.m
+++ inv_cin_psd.m
@@ -1,6 +1,6 @@
 %% Algoritmo per l'inversione cinematica con la pseudo-inversa
-% in uscita da questo algoritmo ho la Qddot ossia la velocit‡ del giunto e
-% lo passo alla funzione analitical per ottenere cosi la q. Bisogner‡
+% in uscita da questo algoritmo ho la Qddot ossia la velocit√† del giunto e
+% lo passo alla funzione analitical per ottenere cosi la q. Bisogner√†
 % quindi nel blocco analitical fare solo l'integrale
 
 function Qddot = inv_cin_psd(p,theta,Q,a1,a2,a3,a4,J)
@@ -20,7 +20,7 @@ function Qddot = inv_cin_psd(p,theta,Q,a1,a2,a3,a4,J)
         K =[kgain 0; 0 kgain];
         % kgain = tot;
         Ka=[1 0; 0 3];
-        % A Ë il det(Ja*(Ja)')         
+        % A √® il det(Ja*(Ja)')         
         A=2*a1^2*a2^2*cos(q1 + q2)^2*sin(q1)^2 + 2*a1^2*a2^2*sin(q1 + q2)^2*cos(q1)^2 ...
             + 2*a2^2*a3^2*cos(q1 + q2 + q3)^2*sin(q1 + q2)^2 + 2*a2^2*a3^2*sin(q1 + q2 + q3)^2*cos(q1 + q2)^2 +...
             2*a1^2*a3^2*cos(q1 + q2 + q3)^2*sin(q1)^2 + 2*a1^2*a3^2*sin(q1 + q2 + q3)^2*cos(q1)^2 - 4*a2^2*a3^2*cos(q1 + q2 + q3)*sin(q1 + q2 + q3)*cos(q1 + q2)*sin(q1 + q2) ...
@@ -30,7 +30,7 @@ function Qddot = inv_cin_psd(p,theta,Q,a1,a2,a3,a4,J)
             - 2*a1^2*a2*a3*sin(q1 + q2 + q3)*cos(q1 + q2)*cos(q1)*sin(q1) - 2*a1*a2*a3^2*cos(q1 + q2 + q3)*sin(q1 + q2 + q3)*cos(q1 + q2)*sin(q1) -...
             2*a1*a2*a3^2*cos(q1 + q2 + q3)*sin(q1 + q2 + q3)*sin(q1 + q2)*cos(q1);
         if(A==0)          
-            Qddot=J_pi*(Xddot+K*e)+P*Ka*dw;% dove Xddot Ë la velocit‡ desiderata dell'organo terminale
+            Qddot=J_pi*(Xddot+K*e)+P*Ka*dw;% dove Xddot √® la velocit√† desiderata dell'organo terminale
         else
             Qddot=J_pi*(Xddot+K*e);
             
@@ -38,7 +38,7 @@ function Qddot = inv_cin_psd(p,theta,Q,a1,a2,a3,a4,J)
 
 
         if(abs(X-[p(1);p(2);theta])<1e-3)
-            Qddot=[q1 q2 q3];  
+            Qddot=[q1 q2 q3 q4];  
         end
     
     end
